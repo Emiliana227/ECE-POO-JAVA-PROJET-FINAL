c56136c9cbecf750a543cb081b5f70c8f0e2800b
Devofmay <prodevrelex@gmail.com>
2025-12-07 00:00:03 +0100

    interface debut

--- JavaProjetFinal/src/main/java/module-info.java
+++ JavaProjetFinal/src/main/java/module-info.java
@@ -1,11 +1,10 @@
-module fr.ece.javaprojetfinal {
+module JavaProjetFinal {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.bootstrapfx.core;
 
     opens fr.ece.javaprojetfinal to javafx.fxml;
+
     exports fr.ece.javaprojetfinal;
+
 }
